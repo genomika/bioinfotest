c1bc55a4f176b4f4fea95b4439eca345624219a8
Marcel Caraciolo <caraciol@gmail.com>
2021-01-22 12:29:25 -0300

    Added file instructions to download

---
+++ data/data_files_download.sh
@@ -0,0 +1,21 @@
+
+#Download the sample file (R1 e R2 FASTQs)
+
+wget -c https://biognmk.s3.amazonaws.com/biotest/510-7-BRCA_S8_L001_R1_001.fastq.gz
+wget -c https://biognmk.s3.amazonaws.com/biotest/510-7-BRCA_S8_L001_R2_001.fastq.gz
+
+
+
+#Download the human genome hg19 fasta file and the indexed file to use with BWA.
+
+#mkdir -p ./bwa
+
+#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta
+#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.fai
+#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.amb
+#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.bwt
+#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.pac
+#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.sa
+#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.ann
+
+
