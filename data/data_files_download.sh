4e1d69c739f8cbcb7cd1bd82642c753b0d837650
Marcel Caraciolo <caraciol@gmail.com>
2021-01-22 12:35:08 -0300

    Fixes in structure

--- data/data_files_download.sh
+++ data/data_files_download.sh
@@ -8,14 +8,14 @@ wget -c https://biognmk.s3.amazonaws.com/biotest/510-7-BRCA_S8_L001_R2_001.fastq
 
 #Download the human genome hg19 fasta file and the indexed file to use with BWA.
 
-#mkdir -p ./bwa
+mkdir -p ./bwa
 
-#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta
-#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.fai
-#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.amb
-#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.bwt
-#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.pac
-#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.sa
-#wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.ann
+wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta
+wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.fai
+wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.amb
+wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.bwt
+wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.pac
+wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.sa
+wget -c https://biognmk.s3.amazonaws.com/biotest/bwa/hg19.fasta.ann
 
 
